db626ad3f3abb06e98d6879b90cf81fe8121c0e7
faseehahmad00 <faseehahmad00@gmail.com>
2022-05-03 01:04:32 +0500

    fixed layout and font sizes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,14 +4,46 @@ import Fade from "react-reveal";
 
 class Header extends Component {
   render() {
-    if (!this.props.data) return null;
+    let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      alpha: [0.6, 0],
+      scale: [0.1, 0.4],
+      position: 'all',
+      color: ['random', '#ff0000'],
+      cross: 'dead',
+      random: 15,
+    };
 
+    if (Math.random() > 0.85) {
+      config = Object.assign(config, {
+        onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(
+            particle.p.x,
+            particle.p.y,
+            particle.radius * 2,
+            particle.radius * 2
+          );
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+        },
+      });
+    }
+    
+    if (!this.props.data) return null;
     const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
+    const bg_styles = ['circle','lines','custom','polygon']
     return (
       <header id="home">
-        <ParticlesBg type="lines" bg={true}/>
+        <ParticlesBg type={bg_styles[Math.floor(Math.random() * bg_styles.length)]} config={config} bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
